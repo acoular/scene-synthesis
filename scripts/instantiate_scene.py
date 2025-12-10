1966822bdbd9284a835f2a799c9187650a2d7741
Art Pelling <a.pelling@tu-berlin.de>
2025-12-10 14:01:30 +0100

    add simple script

---
+++ scripts/instantiate_scene.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import acoular as ac
+import numpy as np
+
+from scene_synthesis import Source, Scene, Microphone
+
+
+microphones = [
+    Microphone(
+        location=np.array([0, 5, 0]),
+    )
+    for x in (-1, 0, 1)
+]
+
+sources = [
+    Source(
+        signal=ac.SineGenerator(freq=f, num_samples=48000, sample_freq=48000),
+        location=np.array([x, 0, 0]),
+    )
+    for (f, x) in zip((200, 600, 1200), (-2, 0, 2), strict=True)
+]
+
+scene = Scene(microphones=microphones, sources=sources)
