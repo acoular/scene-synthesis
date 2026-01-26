e9bed14c4002443ee97b74706e0873022127135f
jtodev <jan@ostendorf.com>
2026-01-26 12:08:18 +0100

    Fix format

--- tests/test_fly_around.py
+++ tests/test_fly_around.py
@@ -1,12 +1,10 @@
-import numpy as np
-import pytest
-import scene_synthesis as synth
 import acoular as ac
+import numpy as np
+import scene_synthesis as synth
 
 
 def test_fly_around_maneuver():
     """Test that scene synthesis matches analytical solution for fly-around maneuver."""
-
     # Test parameters
     c = 343.0
     T = 10
@@ -27,7 +25,7 @@ def test_fly_around_maneuver():
     z = np.zeros(num_points)
     xyz = np.vstack((x, y, z)).T
 
-    traj = ac.Trajectory(points={time: tuple(loc) for time, loc in zip(t, xyz)})
+    traj = ac.Trajectory(points={time: tuple(loc) for time, loc in zip(t, xyz, strict=True)})
     gen = ac.SineGenerator(freq=signal_frequency, num_samples=num_points, sample_freq=num_points / T)
 
     source = synth.Source()
@@ -44,5 +42,5 @@ def test_fly_around_maneuver():
     synth_res = np.concatenate(list(scene.result(num=100))).flatten()
 
     # Assertion
-    msg = "Scene synthesis result does not match analytical solution"
+    msg = 'Scene synthesis result does not match analytical solution'
     np.testing.assert_allclose(synth_res, ana_result, atol=1e-6, err_msg=msg)
